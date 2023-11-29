0abb3f6415de5b7ee7dd0bce5ea9fcc004f78f47
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2023-11-29 14:25:14 +0700

    add alamat and sosmed controller and textformfield

--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -16,93 +16,107 @@ class _RegisterPageState extends State<RegisterPage> {
   TextEditingController namaController = TextEditingController();
   TextEditingController nbiController = TextEditingController();
   TextEditingController kelasController = TextEditingController();
+  TextEditingController alamatController = TextEditingController();
+  TextEditingController sosmedController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Form(
-        key: formKey,
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/register.png',
-                scale: 4,
-              ),
-              Text(
-                'WELCOME',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                'Praktikum PAB 2023',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.white,
-                ),
-              ),
-              textFieldCustom('Masukkan Nama', namaController),
-              textFieldCustom('Masukkan NBI', nbiController),
-              textFieldCustom('Masukkan Kelas Praktikum', kelasController),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 50, right: 50),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (formKey.currentState!.validate()) {
-                      final SharedPreferences prefs =
-                          await SharedPreferences.getInstance();
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: formKey,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/register.png',
+                    scale: 4,
+                  ),
+                  Text(
+                    'WELCOME',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Praktikum PAB 2023',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                  textFieldCustom('Masukkan Nama', namaController),
+                  textFieldCustom('Masukkan NBI', nbiController),
+                  textFieldCustom('Masukkan Kelas Praktikum', kelasController),
+                  textFieldCustom('Masukkan Alamat ', alamatController),
+                  textFieldCustom(
+                      'Masukkan Sosial Media Anda', sosmedController),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (formKey.currentState!.validate()) {
+                          final SharedPreferences prefs =
+                              await SharedPreferences.getInstance();
 
-                      await prefs.setString('nama', namaController.text);
-                      await prefs.setString('nbi', nbiController.text);
-                      await prefs.setString('kelas', kelasController.text);
+                          await prefs.setString('nama', namaController.text);
+                          await prefs.setString('nbi', nbiController.text);
+                          await prefs.setString('kelas', kelasController.text);
+                          await prefs.setString(
+                              'alamat', alamatController.text);
+                          await prefs.setString(
+                              'sosmed', sosmedController.text);
 
-                      // print(namaController.text);
-                      // print(nbiController.text);
-                      // print(kelasController.text);
+                          // print(namaController.text);
+                          // print(nbiController.text);
+                          // print(kelasController.text);
 
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (build) {
-                            return FirstPage();
-                          },
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (build) {
+                                return FirstPage();
+                              },
+                            ),
+                          );
+                        }
+                      },
+                      child: Center(
+                        child: Text(
+                          'Daftar',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      );
-                    }
-                  },
-                  child: Center(
-                    child: Text(
-                      'Daftar',
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.green,
+                        padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
                       ),
                     ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    backgroundColor: Colors.green,
-                    padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
-                  ),
-                ),
-              )
-            ],
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
